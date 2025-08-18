86a6d4321893bff634c94d3b1b26f17398d1bd65
iistarion <iistarion@gmail.com>
2025-08-18 17:59:03 +0200

    Fix code style

--- Assets/LunarPaw.Steering/Demo/Scripts/Scenes/ScenesDescription.cs
+++ Assets/LunarPaw.Steering/Demo/Scripts/Scenes/ScenesDescription.cs
@@ -7,12 +7,12 @@ namespace LunarPaw.Steering.Runtime.Demo.Scenes
     public class ScenesDescription : ScriptableObject
     {
         public SerializedDictionary<string, string> SceneDescriptions = new SerializedDictionary<string, string>
-    {
-        { "Seek", "Demonstrates how boids can seek a target." },
-        { "Arrive", "Shows how boids can arrive at a target." },
-        { "Flee", "Demonstrates how boids can flee from a target." },
-        { "Evade", "Shows how boids can evade a target." },
-        { "Pursue", "Demonstrates how boids can pursue a target." },
-    };
+        {
+            { "Seek", "Demonstrates how boids can seek a target." },
+            { "Arrive", "Shows how boids can arrive at a target." },
+            { "Flee", "Demonstrates how boids can flee from a target." },
+            { "Evade", "Shows how boids can evade a target." },
+            { "Pursue", "Demonstrates how boids can pursue a target." },
+        };
     }
 }
